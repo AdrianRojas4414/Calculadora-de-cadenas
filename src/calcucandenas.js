84bc5c6d07fb62673ccf53bfeb36125b1349ec3a
Adrian Augusto Rojas Ortuño <adrian.rojas.o@ucb.edu.bo>
2024-05-14 09:58:09 -0400

    Refactoring: cambio de nombre de la operacion logica que identifica un comando en el string

--- src/calcucandenas.js
+++ src/calcucandenas.js
@@ -2,13 +2,13 @@ function addNumbersIn(inputString) {
   let defaultDelimiters = [',', '-', /[,|-]/];
   let numbers = inputString;
   let summation = 0;
-  const delimiterMatch = inputString.match(/^\/\/\[(.)\]/);
+  const isComandInString = inputString.match(/^\/\/\[(.)\]/);
 
-  if(delimiterMatch){
-    const delimiter = delimiterMatch[1];
+  if(isComandInString){
+    const delimiter = isComandInString[1];
     defaultDelimiters.pop();
     defaultDelimiters.push(new RegExp(`[${delimiter}|,|-]`));
-    numbers = inputString.slice(delimiterMatch[0].length +3 );
+    numbers = inputString.slice(isComandInString[0].length +3 );
   }
 
   numbers = inputString.split(defaultDelimiters[2]);
