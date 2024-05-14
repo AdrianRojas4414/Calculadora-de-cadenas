67e91872eb04a7e3857316b8282fe36075036605
Adrian Augusto Rojas Ortuño <adrian.rojas.o@ucb.edu.bo>
2024-05-14 11:13:48 -0400

    Refactoring: funcion de sumar los numeros del string fue modificada para que este mas completa

--- src/calcucandenas.js
+++ src/calcucandenas.js
@@ -1,31 +1,37 @@
-function doMultipleSliceWith(defaultDelimiters, comandInString, stringToSplit){
-  const comandDelimiter = comandInString[1];
-  defaultDelimiters.pop();
-  defaultDelimiters.push(new RegExp(`[${comandDelimiter}|,|-]`));
+function removeDelimiterFromString(stringToSplit, comandInString){
   return stringToSplit.slice(comandInString[0].length);
 }
+
+function getNewDelimiter(Delimiters, comandInString){
+  const comandDelimiter = comandInString[1];
+  Delimiters.pop();
+  Delimiters.push(new RegExp(`[${comandDelimiter}|,|-]`));
+  return Delimiters;
+}
+
 function addArray(arrayOfStringNumbers){
   let summation = 0;
-  for(const number of arrayOfStringNumbers){
-    summation+= number <= 1000? Number(number):0;
+  if(Number(arrayOfStringNumbers) === 0)
+    return 0;
+  if(arrayOfStringNumbers.length != 1){
+    for(const number of arrayOfStringNumbers)
+      summation+= number <= 1000? Number(number):0;
+    return summation;
   }
-  return summation;
+  return Number(arrayOfStringNumbers[0]);
 }
 
 function addNumbersIn(inputString) {
   let defaultDelimiters = [',', '-', /[,|-]/];
   let arrayOfStringNumbers;
+
   const isComandInString = inputString.match(/^\/\/\[(.)\]/);
   if(isComandInString){
-    arrayOfStringNumbers = doMultipleSliceWith(defaultDelimiters, isComandInString, inputString);
+    defaultDelimiters = getNewDelimiter(defaultDelimiters, isComandInString);
+    arrayOfStringNumbers = removeDelimiterFromString(inputString, isComandInString);
   }
   arrayOfStringNumbers = inputString.split(defaultDelimiters[2]);
-
-  if(inputString === "")
-    return 0;
-  if(arrayOfStringNumbers.length != 1){
-    return addArray(arrayOfStringNumbers);
-  }
-  return Number(arrayOfStringNumbers[0]);
+  
+  return addArray(arrayOfStringNumbers);
 }
 export default addNumbersIn;
