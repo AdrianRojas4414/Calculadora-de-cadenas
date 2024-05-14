349921b346899080c8285f6d9ec5f5394eed337a
Adrian Augusto Rojas Ortuño <adrian.rojas.o@ucb.edu.bo>
2024-05-14 15:30:45 -0400

    Refactoring: las funciones se cambiaron de lugar para una mayor legilibilidad

--- src/calcucandenas.js
+++ src/calcucandenas.js
@@ -1,26 +1,3 @@
-function removeDelimiterFromString(stringToSplit, comandInString){
-  return stringToSplit.slice(comandInString[0].length);
-}
-
-function getNewDelimiter(Delimiters, comandInString){
-  const comandDelimiter = comandInString[1];
-  Delimiters.pop();
-  Delimiters.push(new RegExp(`[${comandDelimiter}|,|-]`));
-  return Delimiters;
-}
-
-function addArray(arrayOfStringNumbers){
-  let summation = 0;
-  if(Number(arrayOfStringNumbers) === 0)
-    return 0;
-  if(arrayOfStringNumbers.length != 1){
-    for(const number of arrayOfStringNumbers)
-      summation+= number <= 1000? Number(number):0;
-    return summation;
-  }
-  return Number(arrayOfStringNumbers[0]);
-}
-
 function addNumbersIn(inputString) {
   let defaultDelimiters = [',', '-', /[,|-]/];
   let arrayOfStringNumbers;
@@ -33,5 +10,28 @@ function addNumbersIn(inputString) {
   arrayOfStringNumbers = inputString.split(defaultDelimiters[2]);
   
   return addArray(arrayOfStringNumbers);
+}
+
+function getNewDelimiter(Delimiters, comandInString){
+  const comandDelimiter = comandInString[1];
+  Delimiters.pop();
+  Delimiters.push(new RegExp(`[${comandDelimiter}|,|-]`));
+  return Delimiters;
+}
+
+function removeDelimiterFromString(stringToSplit, comandInString){
+  return stringToSplit.slice(comandInString[0].length);
+}
+
+function addArray(arrayOfStringNumbers){
+  let summation = 0;
+  if(Number(arrayOfStringNumbers) === 0)
+    return 0;
+  if(arrayOfStringNumbers.length != 1){
+    for(const number of arrayOfStringNumbers)
+      summation+= number <= 1000? Number(number):0;
+    return summation;
+  }
+  return Number(arrayOfStringNumbers[0]);
 }
 export default addNumbersIn;
